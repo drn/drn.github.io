615cd5b91e7540ee4764e1871123a55eb165d2f3
Darren Cheng <darren@thanx.com>
2019-01-13 23:42:00 -0800

    Add link to github from whoami.

--- src/Executor/Whoami.js
+++ src/Executor/Whoami.js
@@ -4,6 +4,9 @@ import React from 'react'
 import { Runnable } from './Runnable'
 import { Nameable } from './Nameable'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
 class Whoami implements Runnable, Nameable {
   name = 'whoami'
 
@@ -16,6 +19,11 @@ class Whoami implements Runnable, Nameable {
         <div>
           <div>Darren Cheng</div>
           <div>Co-Founder @ Thanx</div>
+          <div>
+            <a href={'https://github.com/drn'} target={'_blank'}>
+              drn @ <FontAwesomeIcon icon={faGithub} />
+            </a>
+          </div>
         </div>
       ),
     }
