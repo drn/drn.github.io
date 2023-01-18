811e8b12d0cd0a440ae9419c6c6eb8e3aad7317c
Darren Cheng <darren@thanx.com>
2023-01-18 01:17:19 -0800

    Replace hardcoded unicode chars with html entities.

--- src/Terminal/Header.tsx
+++ src/Terminal/Header.tsx
@@ -3,6 +3,11 @@ import moment from 'moment'
 import './Header.css'
 import useWindowDimensions from '../hooks/useWindowDimensions'
 
+// &#x2B80; | 11136 - solid right
+// &#x2B81; | 11137 - empty right
+// &#x2B82; | 11138 - solid left
+// &#x2B83; | 11139 - empty left
+
 const Header = () => {
   const [date, setDate] = useState('')
   const [time, setTime] = useState('')
@@ -32,7 +37,11 @@ const Header = () => {
   }
 
   const formatDate: () => string = () => {
-    return moment().format('ddd D ⮃ MMM YYYY')
+    return (
+      moment().format('ddd D ') +
+      String.fromCharCode(11139) +
+      moment().format('MMM YYYY')
+    )
   }
 
   const formatTime: () => string = () => {
@@ -42,37 +51,37 @@ const Header = () => {
   const left =
     width < 800 ? (
       <div className="left">
-        <span className="color1">&nbsp;master ⮁ *&nbsp;</span>
+        <span className="color1">&nbsp;master &#x2B81; *&nbsp;</span>
         <span className="color1_3">⮀</span>
       </div>
     ) : width < 1200 ? (
       <div className="left">
-        <span className="color1">&nbsp;master ⮁ *&nbsp;</span>
-        <span className="color1_3">⮀</span>
+        <span className="color1">&nbsp;master &#x2B81; *&nbsp;</span>
+        <span className="color1_3">&#x2B80;</span>
         <span className="color3"> 192.168.1.1 </span>
       </div>
     ) : (
       <div className="left">
-        <span className="color1">&nbsp;master ⮁ *&nbsp;</span>
-        <span className="color1_2">⮀</span>
+        <span className="color1">&nbsp;master &#x2B81; *&nbsp;</span>
+        <span className="color1_2">&#x2B80;</span>
         <span className="color2">&nbsp;Thanx&nbsp;</span>
-        <span className="color2_3">⮀</span>
+        <span className="color2_3">&#x2B80;</span>
         <span className="color3"> 192.168.1.1 </span>
       </div>
     )
 
   const center = (
     <div className="center">
-      <span className="color3_1">⮀</span>
+      <span className="color3_1">&#x2B80;</span>
       <span className="color1">&nbsp;1 core&nbsp;</span>
-      <span className="color1_3">⮀</span>
+      <span className="color1_3">&#x2B80;</span>
     </div>
   )
 
   const right =
     width < 800 ? (
       <div className="right">
-        <span className="color3_1">⮀</span>
+        <span className="color3_1">&#x2B80;</span>
         <span className="color1">&nbsp;{time}&nbsp;</span>
       </div>
     ) : width < 1200 ? (
@@ -80,7 +89,7 @@ const Header = () => {
         <span className="color3">
           &nbsp;{cpu()} {mem()} {bat()}&nbsp;
         </span>
-        <span className="color3_1">⮀</span>
+        <span className="color3_1">&#x2B80;</span>
         <span className="color1">&nbsp;{time}&nbsp;</span>
       </div>
     ) : (
@@ -88,9 +97,9 @@ const Header = () => {
         <span className="color3">
           &nbsp;{cpu()} {mem()} {bat()}&nbsp;
         </span>
-        <span className="color3_2">⮀</span>
+        <span className="color3_2">&#x2B80;</span>
         <span className="color2">&nbsp;{date}&nbsp;</span>
-        <span className="color2_1">⮀</span>
+        <span className="color2_1">&#x2B80;</span>
         <span className="color1">&nbsp;{time}&nbsp;</span>
       </div>
     )
